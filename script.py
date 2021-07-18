68a46bfe82983457802ef0c067510453bd5ad4da
userr2232 <reynaldo.rz.26@gmail.com>
2021-07-18 17:50:31 -0500

    before execution

---
+++ script.py
@@ -0,0 +1,17 @@
+import subprocess
+from pathlib import Path
+
+subdirs = ["plasma_collision_1", "plasma_collision_2", "plasma_collision_3", "plasma_collision_4"]
+smilei_path = Path("/home/reynaldo.rojas/smilei/Smilei")
+benchmarks_path = Path(smilei_path / "Smilei-benchmarks")
+for subdir in subdirs:
+    i = 8
+    while i <= 64:
+        subprocess.run(f"mkdir {i}_procs".split(), cwd=str(benchmarks_path/subdir))
+        subprocess.run(f"cp plasma_collision.py {i}_procs".split(), cwd=str(benchmarks_path/subdir))
+        subprocess.run(f"cp smilei {i}_procs".split(), cwd=str(benchmarks_path/subdir))
+        # Run with X cores | X particles
+        subprocess.run(f"srun --mpi=pmix_v2 -n {i} ./smilei plasma_collision.py".split(), cwd=str(benchmarks_path/subdir/f"{i}_procs"))
+        i *= 2
+
+
