0a9120da2ee2edaaf0c1b837ad8d94219bf82be2
userr2232 <reynaldo.rz.26@gmail.com>
2021-07-19 21:19:06 -0500

    aa

---
+++ plasma_collision_3/32_procs/job.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH --job-name smilei
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=32
+#SBATCH --mem-per-cpu=1gb
+
+srun --mpi=pmix_v2 /home/reynaldo.rojas/smilei/Smilei/Smilei-benchmarks/plasma_collision_3/32_procs/smilei /home/reynaldo.rojas/smilei/Smilei/Smilei-benchmarks/plasma_collision_3/32_procs/plasma_collision.py
